e9da3f51fc95081f29bcdb8d8768a6dded5c4236
Samantha  Durant <samanthadurant@Samanthas-MacBook-Pro.local>
2025-11-22 11:01:00 -0800

    saved main changes

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { ToDoListApp } from './ToDoListApp/App'
+import { ToDoListApp } from './ToDoListApp/ToDoListApp'
 import './index.css'
 
 createRoot(document.getElementById('root')).render(
